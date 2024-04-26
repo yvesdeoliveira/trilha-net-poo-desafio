2b68a10f23eda911b28158a45f0a84a0c839dcaa
yvesdeoliveira <yves.oliveira@icloud.com>
2024-04-26 11:52:47 -0300

    fix: the missing properties were implemented and the constructor was adjusted

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,22 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public int Imei { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, int imei, int memoria)
+        {
+            // TODO: Passar os parâmetros do construtor para as propriedades
+            Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+        }
+
+        protected Smartphone(string numero)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "3310", imei: 01010101010, memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp Business");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "123789", modelo: "Iphone 15 Pro Max Cabuloso", imei: 1010101010101010, memoria: 512 );
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp Business");
